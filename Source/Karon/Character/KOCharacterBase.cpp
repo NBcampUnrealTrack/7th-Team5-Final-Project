18b720e14b2ef5e4e95535e2f322888e11e9ec1c
minsung <flymin00712@gmail.com>
2026-05-14 20:49:08 +0900

    feat : 베이스 캐릭터 추가

---
+++ Source/Karon/Character/KOCharacterBase.cpp
@@ -0,0 +1,13 @@
+﻿#include "KOCharacterBase.h"
+#include "Karon/AbilitySystem/KOAbilitySystemComponent.h"
+
+AKOCharacterBase::AKOCharacterBase()
+{
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+UAbilitySystemComponent* AKOCharacterBase::GetAbilitySystemComponent() const
+{
+	return AbilitySystemComponent;
+}
+
---
+++ Source/Karon/Character/KOCharacterBase.h
@@ -0,0 +1,24 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystemInterface.h"
+#include "GameFramework/Character.h"
+
+#include "KOCharacterBase.generated.h"
+
+class UKOAbilitySystemComponent; 
+
+UCLASS()
+class KARON_API AKOCharacterBase : public ACharacter, public IAbilitySystemInterface
+{
+	GENERATED_BODY()
+
+public:
+	AKOCharacterBase();
+	
+	virtual UAbilitySystemComponent* GetAbilitySystemComponent() const override;
+	
+protected:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Abilities")
+	TObjectPtr<UKOAbilitySystemComponent> AbilitySystemComponent;
+};
