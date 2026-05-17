904d9c6082f47b676c3bfc2ca1ac4f06aaaec803
minsung <flymin00712@gmail.com>
2026-05-17 21:42:05 +0900

    feat : 캐릭터 무브먼트 컴포넌트 추가

---
+++ Source/Karon/Component/KOCharacterMovementComponent.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "KOCharacterMovementComponent.generated.h"
+
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class KARON_API UKOCharacterMovementComponent : public UCharacterMovementComponent
+{
+	GENERATED_BODY()
+protected:
+	UKOCharacterMovementComponent();
+	
+	virtual void OnMovementModeChanged(EMovementMode PreviousMovementMode, uint8 PreviousCustomMode) override;
+	
+};
