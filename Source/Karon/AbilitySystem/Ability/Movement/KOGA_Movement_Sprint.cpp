a64313af177cf6eb89baeb7aa05445e3a9418270
minsung <flymin00712@gmail.com>
2026-05-17 21:41:30 +0900

    feat: 달리기 어빌리티 추가

---
+++ Source/Karon/AbilitySystem/Ability/Movement/KOGA_Movement_Sprint.cpp
@@ -0,0 +1,176 @@
+﻿#include "KOGA_Movement_Sprint.h"
+
+#include "Abilities/Tasks/AbilityTask_WaitAttributeChange.h"
+#include "AbilitySystem/Attribute/KOStaminaSet.h"
+#include "GameFramework/Character.h"
+#include "GameFramework/CharacterMovementComponent.h"
+
+UKOGA_Movement_Sprint::UKOGA_Movement_Sprint()
+{
+	InstancingPolicy = EGameplayAbilityInstancingPolicy::InstancedPerActor;
+
+}
+
+bool UKOGA_Movement_Sprint::CanActivateAbility(
+	const FGameplayAbilitySpecHandle Handle,
+	const FGameplayAbilityActorInfo* ActorInfo, 
+	const FGameplayTagContainer* SourceTags,
+	const FGameplayTagContainer* TargetTags,
+	FGameplayTagContainer* OptionalRelevantTags) const
+{
+	if (!Super::CanActivateAbility(Handle, ActorInfo, SourceTags, TargetTags, OptionalRelevantTags)) return false;
+	
+	ACharacter* Character = Cast<ACharacter>(GetAvatarActorFromActorInfo());
+	if (!Character) return false;
+	
+	UCharacterMovementComponent* CharacterMovement = Character->GetCharacterMovement(); 
+	if (!CharacterMovement) return false;
+	
+	// 떨어지는 경우 
+	if (CharacterMovement->IsFalling()) return false; 
+	
+	// 움직이지 않는 경우 
+	const FVector Velocity = CharacterMovement->Velocity;
+	const float CurrentSpeed = Velocity.Size2D();
+	if (CurrentSpeed <= 30.f) return false;
+	
+	return true; 
+}
+
+void UKOGA_Movement_Sprint::ActivateAbility(
+	const FGameplayAbilitySpecHandle Handle,
+	const FGameplayAbilityActorInfo* ActorInfo, 
+	const FGameplayAbilityActivationInfo ActivationInfo,
+	const FGameplayEventData* TriggerEventData)
+{
+	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
+	
+	if (!CommitAbility(Handle, ActorInfo, ActivationInfo))
+	{
+		EndAbility(Handle, ActorInfo, ActivationInfo, true, true);
+		return;
+	}
+	
+	// 1. Info 확인 
+	CachedCharacter = GetAvatarCharacter(); 
+	if (!CachedCharacter)
+	{
+		EndAbility(Handle, ActorInfo, ActivationInfo, true, true);
+		return;
+	}
+		
+	CachedMovement = CachedCharacter->GetCharacterMovement(); 
+	if (!CachedMovement)
+	{
+		EndAbility(Handle, ActorInfo, ActivationInfo, true, true);
+		return;
+	}
+	
+	// 2. Effect 적용 
+	if (SprintEffect)
+	{
+		SprintEffectHandle = ApplyEffectToSelf(SprintEffect);
+	}
+	
+	if (SprintCostEffect)
+	{
+		SprintCostEffectHandle = ApplyEffectToSelf(SprintCostEffect);
+	}
+	
+	// 3. Stamina 감소시 마다 달리기 조건 체크 Task 
+	UAbilityTask_WaitAttributeChange* CheckStaminaTask =
+		UAbilityTask_WaitAttributeChange::WaitForAttributeChange(
+			this,
+			UKOStaminaSet::GetStaminaAttribute(),
+			FGameplayTag::EmptyTag,
+			FGameplayTag::EmptyTag,
+			false
+		);
+	
+	if (CheckStaminaTask)
+	{
+		CheckStaminaTask->OnChange.AddDynamic(this, &ThisClass::OnStaminaChanged);
+		CheckStaminaTask->ReadyForActivation(); 
+	}
+}
+
+void UKOGA_Movement_Sprint::EndAbility(
+	const FGameplayAbilitySpecHandle Handle,
+	const FGameplayAbilityActorInfo* ActorInfo,
+	const FGameplayAbilityActivationInfo ActivationInfo,
+	bool bReplicateEndAbility, bool bWasCancelled)
+{
+	GetWorld()->GetTimerManager().ClearTimer(GraceTimer);
+	
+	if (SprintEffectHandle.IsValid())
+	{
+		BP_RemoveGameplayEffectFromOwnerWithHandle(SprintEffectHandle);
+	}
+	if (SprintCostEffectHandle.IsValid())
+	{
+		BP_RemoveGameplayEffectFromOwnerWithHandle(SprintCostEffectHandle);
+	}
+	
+	CachedCharacter = nullptr;
+	CachedMovement = nullptr;
+	
+	Super::EndAbility(Handle, ActorInfo, ActivationInfo, bReplicateEndAbility, bWasCancelled);
+}
+
+void UKOGA_Movement_Sprint::InputReleased(
+	const FGameplayAbilitySpecHandle Handle,
+	const FGameplayAbilityActorInfo* ActorInfo,
+	const FGameplayAbilityActivationInfo ActivationInfo)
+{
+	Super::InputReleased(Handle, ActorInfo, ActivationInfo);
+	
+	if (IsActive())
+	{
+		EndAbility(Handle, ActorInfo, ActivationInfo, true, false);
+	}
+}
+
+void UKOGA_Movement_Sprint::OnStaminaChanged()
+{
+	if (!CachedCharacter || !CachedMovement) return; 
+	
+	// 스테미나가 체크 
+	const float CurrentStamina = GetASC()->GetNumericAttribute(UKOStaminaSet::GetStaminaAttribute());
+	if (CurrentStamina <= 0.01f)
+	{
+		EndAbility(CurrentSpecHandle, CurrentActorInfo, CurrentActivationInfo, true, true);
+		return;
+	}
+	
+	// 캐릭터가 멈추거나 떨어지는 경우 : 유예 
+	if (CachedMovement->GetCurrentAcceleration().IsNearlyZero() || CachedMovement->IsFalling())
+	{
+		TryStartGraceTimer();
+		return; 
+	}
+	
+	GetWorld()->GetTimerManager().ClearTimer(GraceTimer);
+}
+
+void UKOGA_Movement_Sprint::TryStartGraceTimer()
+{
+	if (GetWorld()->GetTimerManager().IsTimerActive(GraceTimer)) return;
+	
+	TWeakObjectPtr<UKOGA_Movement_Sprint> WeakThis(this);
+	GetWorld()->GetTimerManager().SetTimer(
+		GraceTimer,
+		[WeakThis]()
+		{
+			if (WeakThis.IsValid())
+				WeakThis->EndAbility(
+					WeakThis->CurrentSpecHandle,
+					WeakThis->CurrentActorInfo,
+					WeakThis->CurrentActivationInfo,
+					true, false
+				);
+		},
+		GraceTime
+		, false
+	);
+}
+
