7c387470b0583f42b7cc31bd3eeb537c66b42822
minsung <flymin00712@gmail.com>
2026-05-18 16:00:07 +0900

    feat: 플레이어 캐릭터 추가

---
+++ Source/Karon/Character/Hero/KOHeroCharacter.cpp
@@ -0,0 +1,38 @@
+﻿#include "KOHeroCharacter.h"
+
+#include "AbilitySystem/KOAbilitySystemComponent.h"
+#include "AbilitySystem/Attribute/KOCombatSet.h"
+#include "AbilitySystem/Attribute/KOStaminaSet.h"
+
+
+AKOHeroCharacter::AKOHeroCharacter(const FObjectInitializer& ObjectInitializer)
+	:Super(ObjectInitializer)
+{
+	PrimaryActorTick.bCanEverTick = true;
+	
+	StaminaSet = CreateDefaultSubobject<UKOStaminaSet>(FName("StaminaSet"));
+	CombatSet = CreateDefaultSubobject<UKOCombatSet>(FName("CombatSet"));
+}
+
+
+void AKOHeroCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void AKOHeroCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	
+	if (AbilitySystemComponent)
+	{
+		AbilitySystemComponent->ProcessAbilityInput(DeltaTime, false);
+	}
+}
+
+void AKOHeroCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+}
+
---
+++ Source/Karon/Character/Hero/KOHeroCharacter.h
@@ -0,0 +1,32 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Character/KOCharacterBase.h"
+#include "KOHeroCharacter.generated.h"
+
+class UKOStaminaSet; 
+class UKOCombatSet;
+
+UCLASS()
+class KARON_API AKOHeroCharacter : public AKOCharacterBase
+{
+	GENERATED_BODY()
+
+public:
+	AKOHeroCharacter(const FObjectInitializer& ObjectInitializer);
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	virtual void Tick(float DeltaTime) override;
+	
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+	
+protected:
+	UPROPERTY()
+	TObjectPtr<UKOStaminaSet> StaminaSet;
+	
+	UPROPERTY()
+	TObjectPtr<UKOCombatSet> CombatSet;
+};
