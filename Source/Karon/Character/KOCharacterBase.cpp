ced074f3364d78c5074e9c75b5f8c58227ba3e25
minsung <flymin00712@gmail.com>
2026-05-18 15:30:26 +0900

    feat: MovementSet Character 바인딩

--- Source/Karon/Character/KOCharacterBase.cpp
+++ Source/Karon/Character/KOCharacterBase.cpp
@@ -1,14 +1,20 @@
 ﻿#include "KOCharacterBase.h"
-
-#include "GMRouterSubsystem.h"
+#include "AbilitySystem/Attribute/KOHealthSet.h"
+#include "AbilitySystem/Attribute/KOMovementSet.h"
+#include "AbilitySystem/Tag/KOGameplayTags.h"
+#include "Component/KOCharacterMovementComponent.h"
 #include "GameFramework/CharacterMovementComponent.h"
 #include "Karon/AbilitySystem/KOAbilitySystemComponent.h"
 
 
 AKOCharacterBase::AKOCharacterBase(const FObjectInitializer& ObjectInitializer)
+	:Super(ObjectInitializer.SetDefaultSubobjectClass<UKOCharacterMovementComponent>(
+		ACharacter::CharacterMovementComponentName))
 {
 	PrimaryActorTick.bCanEverTick = true;
 	
+	HealthSet = CreateDefaultSubobject<UKOHealthSet>(FName("HealthSet"));
+	MovementSet = CreateDefaultSubobject<UKOMovementSet>(FName("MovementSet"));
 }
 
 UAbilitySystemComponent* AKOCharacterBase::GetAbilitySystemComponent() const
@@ -20,16 +26,25 @@ void AKOCharacterBase::BeginPlay()
 {
 	Super::BeginPlay();
 	
-	 BindMovementSet();
+	BindMovementSet();
 }
 
 void AKOCharacterBase::BindMovementSet()
 {
-	//UGMRouterSubsystem::Subscribe(GetWorld(),Tag,FTypeStruct)
+	MovementSet->OnMoveSpeedChanged.AddDynamic(this, &ThisClass::OnMoveSpeedChanged);
+	OnMoveSpeedChanged(600.f, MovementSet->GetMoveSpeed());
+	
+	MovementSet->OnJumpStrengthChanged.AddDynamic(this, &ThisClass::OnJumpStrengthChanged);
+	OnJumpStrengthChanged(420, MovementSet->GetJumpStrength());
 }
 
-void AKOCharacterBase::OnWalkSpeedChanged(float NewWalkSpeed, float OldWalkSpeed)
+void AKOCharacterBase::OnMoveSpeedChanged(float OldWalkSpeed, float NewWalkSpeed)
 {
 	GetCharacterMovement()->MaxWalkSpeed = NewWalkSpeed;
+}
+
+void AKOCharacterBase::OnJumpStrengthChanged(float OldJumpStrength, float NewJumpStrength)
+{
+	GetCharacterMovement()->JumpZVelocity = NewJumpStrength;
 }
 
