95ed23d75ebc983c9917b564906bce19f3df1437
NMEii <skaxo0823@gmail.com>
2026-05-20 17:36:23 +0900

    add: Log 카테고리 선언 파일 추가

---
+++ Source/Karon/Log/KOLogCategory.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Logging/LogMacros.h"
+
+DECLARE_LOG_CATEGORY_EXTERN(Log_YS,     Log, All);  // 신영섭 (공장 파트장)
+DECLARE_LOG_CATEGORY_EXTERN(Log_KMS,    Log, All);  // 권민성 (플레이어 파트장)
+DECLARE_LOG_CATEGORY_EXTERN(Log_KHS,    Log, All);  // 김현석 (몬스터 파트장)
+DECLARE_LOG_CATEGORY_EXTERN(Log_KTW,    Log, All);  // 김태우
+DECLARE_LOG_CATEGORY_EXTERN(Log_KNT,    Log, All);  // 김남태
+DECLARE_LOG_CATEGORY_EXTERN(Log_JYN,    Log, All);  // 정유나
+DECLARE_LOG_CATEGORY_EXTERN(Log_CSH,    Log, All);  // 최성훈
+DECLARE_LOG_CATEGORY_EXTERN(Log_PDN,    Log, All);  // 박둘내
+
