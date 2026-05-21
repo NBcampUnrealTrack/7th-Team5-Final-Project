fa07c27ac3fc6b3ab16c02c5302550d3ca7db66c
minsung <flymin00712@gmail.com>
2026-05-21 11:49:57 +0900

    feedback: 락온 기능 리뷰

--- Source/Karon/AbilitySystem/Ability/KOGA_LockOn.h
+++ Source/Karon/AbilitySystem/Ability/KOGA_LockOn.h
@@ -1,21 +1,19 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
-#include "Abilities/GameplayAbility.h"
-#include "KOLockOnGameplayAbility.generated.h"
+#include "KOGameplayAbilityBase.h"
+#include "KOGA_LockOn.generated.h"
 
 /**
  * 
  */
 UCLASS()
-class KARON_API UKOLockOnGameplayAbility : public UGameplayAbility
+class KARON_API UKOGA_LockOn : public UKOGameplayAbilityBase
 {
 	GENERATED_BODY()
 	
 public:
-	UKOLockOnGameplayAbility();
+	UKOGA_LockOn();
 
 	virtual void ActivateAbility(
 		const FGameplayAbilitySpecHandle Handle,
