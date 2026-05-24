660cef64c14a65e9575ac9b1c18adb5540e9011a
csh8696-git <csh8696@gmail.com>
2026-05-24 23:01:38 +0900

    fix : 멍청 휴면에러 수정

--- Source/Karon/UI/KOTitleRootLayout.cpp
+++ Source/Karon/UI/KOTitleRootLayout.cpp
@@ -9,10 +9,16 @@ void UKOTitleRootLayout::NativeConstruct()
 	Super::NativeConstruct();
 	
 	ULocalPlayer* LocalPlayer = GetOwningLocalPlayer();
-	if (LocalPlayer) return;
+	if (LocalPlayer == nullptr)
+	{
+		return;
+	}
 
 	UKOUISubsystem* UISubsystem = LocalPlayer->GetSubsystem<UKOUISubsystem>();
-	if (UISubsystem) return;
+	if (UISubsystem == nullptr)
+	{
+		return;
+	}
 	// 1. 메인 메뉴용 태그와 매핑하여 내부 컨테이너 컴포넌트를 등록!
 	if (TitleLayer)
 	{
