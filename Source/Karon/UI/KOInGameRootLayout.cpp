bbcdb1c80eed6b0d7336ad71bafe1f7e61397016
HumilityShin <quantum0724@gmail.com>
2026-05-25 23:31:35 +0900

    feat: InGameRoot Layout Init

---
+++ Source/Karon/UI/KOInGameRootLayout.cpp
@@ -0,0 +1,38 @@
+// Copyright Karon Team 5. All Rights Reserved.
+
+#include "KOInGameRootLayout.h"
+
+#include "KOUISubsystem.h"
+#include "AbilitySystem/Tag/KOGameplayTags.h"
+
+#include "Widgets/CommonActivatableWidgetContainer.h"
+
+void UKOInGameRootLayout::NativeConstruct()
+{
+    Super::NativeConstruct();
+
+    ULocalPlayer* LocalPlayer = GetOwningLocalPlayer();
+    if (LocalPlayer == nullptr)
+    {
+        return;
+    }
+
+    UKOUISubsystem* UISubsystem = LocalPlayer->GetSubsystem<UKOUISubsystem>();
+    if (UISubsystem == nullptr)
+    {
+        return;
+    }
+
+    if (GameLayer)
+    {
+        UISubsystem->RegisterPrimaryLayout(KOGameplayTags::UI_Layer_Game, GameLayer);
+    }
+    if (GameMenuLayer)
+    {
+        UISubsystem->RegisterPrimaryLayout(KOGameplayTags::UI_Layer_GameMenu, GameMenuLayer);
+    }
+    if (ModalLayer)
+    {
+        UISubsystem->RegisterPrimaryLayout(KOGameplayTags::UI_Layer_Modal, ModalLayer);
+    }
+}
