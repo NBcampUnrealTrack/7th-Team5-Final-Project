b325dd8b9eb6dfc31382029f116c7713a4e4eee3
Kim Taewoo <xodn246@gmail.com>
2026-05-26 16:22:29 +0900

    fix: 파일 이름 변경

--- Source/Karon/Character/Enemy/Boss/BTService_BossDetectPlayer.cpp
+++ Source/Karon/Character/Enemy/Boss/BTService_BossDetectPlayer.cpp
@@ -1,19 +1,19 @@
-#include "Character/Enemy/Boss/KOBTService_BossDetectPlayer.h"
+#include "Character/Enemy/Boss/BTService_BossDetectPlayer.h"
 
 #include "AIController.h"
-#include "KOAIC_Boss.h"
+#include "KOAIC_BossChapter01.h"
 #include "BehaviorTree/BlackboardComponent.h"
 #include "GameFramework/Character.h"
 #include "Kismet/GameplayStatics.h"
 
 
-UKOBTService_BossDetectPlayer::UKOBTService_BossDetectPlayer()
+UBTService_BossDetectPlayer::UBTService_BossDetectPlayer()
 {
 	Interval = 0.2f;
 	NodeName = TEXT("Detect Player");
 }
 
-void UKOBTService_BossDetectPlayer::TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds)
+void UBTService_BossDetectPlayer::TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds)
 {
 	Super::TickNode(OwnerComp, NodeMemory, DeltaSeconds);
 	
@@ -45,16 +45,13 @@ void UKOBTService_BossDetectPlayer::TickNode(UBehaviorTreeComponent& OwnerComp, 
 	
 	if (Distance <= TraceRange)
 	{
-		BB->SetValueAsObject(AKOAIC_Boss::TargetActorKey, PlayerCharacter);
+		BB->SetValueAsObject(AKOAIC_BossChapter01::TargetActorKey, PlayerCharacter);
 	}
 	else
 	{
-		BB->SetValueAsObject(AKOAIC_Boss::TargetActorKey, nullptr);
+		BB->SetValueAsObject(AKOAIC_BossChapter01::TargetActorKey, nullptr);
 	}
 	
 	// 추적거리 체크
-	BB->SetValueAsBool(AKOAIC_Boss::bIsInTraceRangeKey,Distance <= TraceRange);
- 
-	// 공격거리 체크
-	BB->SetValueAsBool(AKOAIC_Boss::bIsInAttackRangeKey,Distance <= AttackRange);
+	BB->SetValueAsBool(AKOAIC_BossChapter01::bIsInTraceRangeKey,Distance <= TraceRange);
 }
--- Source/Karon/Character/Enemy/Boss/BTService_BossDetectPlayer.h
+++ Source/Karon/Character/Enemy/Boss/BTService_BossDetectPlayer.h
@@ -2,22 +2,19 @@
 
 #include "CoreMinimal.h"
 #include "BehaviorTree/BTService.h"
-#include "KOBTService_BossDetectPlayer.generated.h"
+#include "BTService_BossDetectPlayer.generated.h"
 
 UCLASS()
-class KARON_API UKOBTService_BossDetectPlayer : public UBTService
+class KARON_API UBTService_BossDetectPlayer : public UBTService
 {
 	GENERATED_BODY()
 	
 public:
-	UKOBTService_BossDetectPlayer();
+	UBTService_BossDetectPlayer();
 	
 protected:
 	virtual void TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds) override;
 	
 	UPROPERTY(EditAnywhere, Category="Detection")
 	float TraceRange = 1500.f;
-	
-	UPROPERTY(EditAnywhere, Category="Detection")
-	float AttackRange = 500.f;
 };
