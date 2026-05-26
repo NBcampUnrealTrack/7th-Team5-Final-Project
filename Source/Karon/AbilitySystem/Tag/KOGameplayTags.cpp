a23d6088f3412b4b7d6f864bfa1b640e6e392a82
Kim Taewoo <xodn246@gmail.com>
2026-05-26 09:47:34 +0900

    feat: 보스 태그 추가

--- Source/Karon/AbilitySystem/Tag/KOGameplayTags.cpp
+++ Source/Karon/AbilitySystem/Tag/KOGameplayTags.cpp
@@ -105,6 +105,7 @@ namespace KOGameplayTags
 	UE_DEFINE_GAMEPLAY_TAG(Attack_Boss01_Normal01, "Attack.Boss01.Normal01");
 	UE_DEFINE_GAMEPLAY_TAG(Attack_Boss01_Normal02, "Attack.Boss01.Normal02");
 	UE_DEFINE_GAMEPLAY_TAG(Attack_Boss01_Strong01, "Attack.Boss01.Strong01");
+
 	
 	UE_DEFINE_GAMEPLAY_TAG(State_Attacking, "State.Attacking");
 	UE_DEFINE_GAMEPLAY_TAG(State_Groggy, "State.Groggy");
