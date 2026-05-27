1699ff6dd6c11b2a95c25541ede3af0f860d1743
khs081215 <khs081215@naver.com>
2026-05-27 20:49:52 +0900

    feat: init Enemy Cluster
    1) 일반 몬스터 군집을 제작하였습니다

---
+++ Source/Karon/Character/Enemy/Cluster/KOEnemyCluster.cpp
@@ -0,0 +1,109 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "KOEnemyCluster.h"
+
+#include "NavigationSystem.h"
+#include "Character/Enemy/KOBaseEnemy.h"
+#include "Kismet/KismetMathLibrary.h"
+
+
+// Sets default values
+AKOEnemyCluster::AKOEnemyCluster()
+{
+	Scene = CreateDefaultSubobject<USceneComponent>(TEXT("Scene"));
+	SetRootComponent(Scene);
+    
+	SpawningBox = CreateDefaultSubobject<UBoxComponent>(TEXT("SpawningBox"));
+	SpawningBox->SetupAttachment(Scene);
+}
+
+// Called when the game starts or when spawned
+void AKOEnemyCluster::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	SpawnEnemies();
+}
+
+
+void AKOEnemyCluster::SpawnEnemies()
+{
+	UNavigationSystemV1* NavSystem = FNavigationSystem::GetCurrent<UNavigationSystemV1>(GetWorld());
+	if (NavSystem==nullptr)
+	{
+		return;
+	}
+	
+	const FVector BoxExtent = SpawningBox->GetScaledBoxExtent();
+	const FVector BoxOrigin = SpawningBox->GetComponentLocation();
+	
+	//이미 스폰된 위치를 저장
+	TArray<FVector> SpawnedLocations;
+	
+	for (auto EnemyPair : EnemyMap)
+	{
+		for (int32 i=0;i<EnemyPair.Value;i++)
+		{
+			//스폰가능한 표본이 있는지 여부
+			bool bValidPointFound = false;
+			//표본이 될 수 있는 후보 포인트
+			FVector CandidatePoint = FVector::ZeroVector;
+			
+			//기존 표본과 일정 이상 거리를 두는 다른 표본 획득을 해당 횟수만큼 시도
+			for (int32 j=0;j<MaxAttemptsPerPoint;j++)
+			{
+				//Volume내 포인트를 가져온다.
+				FVector RandomPointInVolume = UKismetMathLibrary::RandomPointInBoundingBox(BoxOrigin, BoxExtent);
+				FNavLocation NavLocation;
+				//Projection 범위
+				FVector QueryExtent(ProjectionDistance, ProjectionDistance, BoxExtent.Z);
+				
+				//NavMesh로의 투영이 가능한 경우
+				if (NavSystem->ProjectPointToNavigation(RandomPointInVolume, NavLocation, QueryExtent))
+				{
+					CandidatePoint = NavLocation.Location;
+					bool bTooClose = false;
+
+					//거리 검사
+					for (auto& ExistingLoc : SpawnedLocations)
+					{
+						//거리가 가까운 경우 탈락
+						if (FVector::DistSquaredXY(CandidatePoint, ExistingLoc) < FMath::Square(MinDistanceBetweenEnemies))
+						{
+							bTooClose = true;
+							break;
+						}
+					}
+					//기존의 모든 표본보다 MinDistanceBetweenEnemies이상 멀 경우
+					if (!bTooClose)
+					{
+						bValidPointFound = true;
+						break;
+					}
+				}
+			}
+			//스폰가능한 표본을 찾은 경우
+			if (bValidPointFound)
+			{
+				//기존 TArray에 추가
+				SpawnedLocations.Add(CandidatePoint);
+				//다른 액터들과 겹칠 경우 충돌을 피해서 반드시 스폰
+				FActorSpawnParameters SpawnParams;
+				SpawnParams.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AdjustIfPossibleButAlwaysSpawn;
+				
+				
+				//TODO: 스폰 대신 풀링으로 관리
+				//랜덤한 방향으로 스폰
+				FRotator RandomRotation(0.f, UKismetMathLibrary::RandomFloatInRange(0.f, 360.f), 0.f);
+				//Z오프셋을 더해 Enemy가 바닥에 끼는 현상 방지
+				CandidatePoint=CandidatePoint+FVector(0,0,EnemyZOffset);
+				GetWorld()->SpawnActor<AKOBaseEnemy>(EnemyPair.Key, CandidatePoint, RandomRotation, SpawnParams);
+			}
+		}
+		
+		
+	}
+	
+	
+}
---
+++ Source/Karon/Character/Enemy/Cluster/KOEnemyCluster.h
@@ -0,0 +1,50 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/BoxComponent.h"
+#include "GameFramework/Actor.h"
+#include "KOEnemyCluster.generated.h"
+
+class AKOBaseEnemy;
+
+UCLASS()
+class KARON_API AKOEnemyCluster : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	AKOEnemyCluster();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	
+	
+private:
+	void SpawnEnemies();
+	
+protected:
+	UPROPERTY(EditAnywhere,Category="Enemy|Map")
+	TMap<TSubclassOf<AKOBaseEnemy>,int32> EnemyMap;
+	
+	UPROPERTY(EditAnywhere)
+	TObjectPtr<UBoxComponent> SpawningBox;
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Spawning")
+	USceneComponent* Scene;
+	
+	//푸아송 디스크의 표본 사이 최소 거리
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Spawn Settings")
+	float MinDistanceBetweenEnemies = 300.f;
+	
+	//푸아송 디스크의 표본 추출 시도 최대 횟수
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Spawn Settings")
+	int32 MaxAttemptsPerPoint = 30;
+	
+private:
+	float ProjectionDistance=200.f;
+	float EnemyZOffset=90.f;
+};
