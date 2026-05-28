d61e77736f05f170bc225f30f4646888f079e7ad
minsung <flymin00712@gmail.com>
2026-05-28 15:45:26 +0900

    feat: HitReaction 구현
    - 이름 변경 (HitReaction->HitReact)
    - HitStop

---
+++ Source/Karon/AbilitySystem/Ability/AbilityTask/AbilityTask_HitStop.h
@@ -0,0 +1,42 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Abilities/Tasks/AbilityTask.h"
+#include "AbilityTask_HitStop.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FHitStopFinishedDelegate);
+
+UCLASS()
+class KARON_API UAbilityTask_HitStop : public UAbilityTask
+{
+	GENERATED_BODY()
+	
+public:
+	UFUNCTION(BlueprintCallable, Category="Ability|Tasks", meta=(HidePin="OwningAbility", DefaultToSelf="OwningAbility"))
+	static UAbilityTask_HitStop* HitStop(
+		UGameplayAbility* OwningAbility,
+		float Duration          = 0.06f,
+		float TimeDilation      = 0.0f,
+		bool  bAffectInstigator = true
+	);
+	
+	virtual void Activate() override;
+	
+private:
+	void RecoverTime();
+	
+public:
+	UPROPERTY(BlueprintAssignable)
+	FHitStopFinishedDelegate OnFinished;
+	
+private:
+	float StopDuration;
+	float StopDilation;
+	bool  bAffectInstigatorToo;
+	FTimerHandle RecoveryTimer;
+	
+	
+	TWeakObjectPtr<AActor> CachedAvatar;
+	TWeakObjectPtr<AActor> CachedInstigator;
+
+};
