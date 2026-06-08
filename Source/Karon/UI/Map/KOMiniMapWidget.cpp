10e715a6cc069bcc8f1a400a4cb5352e4ba258cd
Einside0227 <moung051@naver.com>
2026-06-08 16:59:01 +0900

    feat: 미니맵 기능 추가

---
+++ Source/Karon/UI/Map/KOMiniMapWidget.cpp
@@ -0,0 +1,97 @@
+﻿#include "UI/Map/KOMiniMapWidget.h"
+
+#include "Components/Image.h"
+#include "Components/SizeBox.h"
+#include "GameFramework/PlayerController.h"
+#include "GameFramework/Pawn.h"
+#include "Materials/MaterialInstanceDynamic.h"
+#include "KOMapUIComponent.h"
+
+UKOMiniMapWidget::UKOMiniMapWidget()
+{
+    InputMode = EKOUIInputMode::Game;
+    bIsBackHandler = false;
+}
+
+void UKOMiniMapWidget::SetMapUIComponent(UKOMapUIComponent* InMapUIComponent)
+{
+    MapUIComponent = InMapUIComponent;
+
+    CacheMapData();
+    ApplyMaterialParams();
+}
+
+void UKOMiniMapWidget::NativePreConstruct()
+{
+    Super::NativePreConstruct();
+
+    if (!MiniMapMaterial || !ImageMiniMapTexture)
+    {
+        return;
+    }
+
+    MiniMapMID = UMaterialInstanceDynamic::Create(MiniMapMaterial, this);
+    ImageMiniMapTexture->SetBrushFromMaterial(MiniMapMID);
+}
+
+void UKOMiniMapWidget::NativeConstruct()
+{
+    Super::NativeConstruct();
+
+    CacheMapData();
+    ApplyMaterialParams();
+}
+
+void UKOMiniMapWidget::CacheMapData()
+{
+    if (!MapUIComponent)
+    {
+        return;
+    }
+
+    MapTexture = MapUIComponent->GetMapTexture();
+    LevelDimension = MapUIComponent->GetLevelDimension();
+    MiniMapZoom = MapUIComponent->GetMiniMapZoom();
+}
+
+void UKOMiniMapWidget::ApplyMaterialParams()
+{
+    if (!MiniMapMID)
+    {
+        return;
+    }
+
+    MiniMapMID->SetTextureParameterValue(TEXT("Map Texture"), MapTexture);
+    MiniMapMID->SetScalarParameterValue(TEXT("Zoom"), MiniMapZoom);
+    MiniMapMID->SetScalarParameterValue(TEXT("Dimension"), LevelDimension);
+}
+
+void UKOMiniMapWidget::NativeTick(const FGeometry& MyGeometry, float InDeltaTime)
+{
+    Super::NativeTick(MyGeometry, InDeltaTime);
+
+    UpdateMiniMap();
+}
+
+void UKOMiniMapWidget::UpdateMiniMap()
+{
+    if (!MiniMapMID || !ImagePlayerArrow)
+    {
+        return;
+    }
+
+    APlayerController* PC = GetOwningPlayer();
+    APawn* Pawn = PC ? PC->GetPawn() : nullptr;
+    if (!Pawn)
+    {
+        return;
+    }
+
+    const FVector Location = Pawn->GetActorLocation();
+    const FRotator Rotation = Pawn->GetActorRotation();
+
+    MiniMapMID->SetScalarParameterValue(TEXT("X"), Location.X);
+    MiniMapMID->SetScalarParameterValue(TEXT("Y"), Location.Y);
+
+    ImagePlayerArrow->SetRenderTransformAngle(Rotation.Yaw);
+}
---
+++ Source/Karon/UI/Map/KOMiniMapWidget.h
@@ -0,0 +1,57 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "UI/KOActivatableWidget.h"
+#include "KOMiniMapWidget.generated.h"
+
+class UImage;
+class USizeBox;
+class UMaterialInterface;
+class UMaterialInstanceDynamic;
+class UTexture2D;
+class UKOMapUIComponent;
+
+UCLASS(Abstract, BlueprintType, Blueprintable)
+class KARON_API UKOMiniMapWidget : public UKOActivatableWidget
+{
+	GENERATED_BODY()
+
+public:
+	UKOMiniMapWidget();
+
+	void SetMapUIComponent(UKOMapUIComponent* InMapUIComponent);
+
+protected:
+	virtual void NativePreConstruct() override;
+	virtual void NativeConstruct() override;
+	virtual void NativeTick(const FGeometry& MyGeometry, float InDeltaTime) override;
+
+	UPROPERTY(meta=(BindWidget))
+	TObjectPtr<UImage> ImageMiniMapTexture;
+
+	UPROPERTY(meta=(BindWidget))
+	TObjectPtr<UImage> ImagePlayerArrow;
+
+	UPROPERTY(meta=(BindWidgetOptional))
+	TObjectPtr<USizeBox> SizeBoxMiniMap;
+
+	UPROPERTY(EditDefaultsOnly, Category="KO|Map")
+	TObjectPtr<UMaterialInterface> MiniMapMaterial;
+
+private:
+	void CacheMapData();
+	void ApplyMaterialParams();
+	void UpdateMiniMap();
+
+	UPROPERTY()
+	TObjectPtr<UKOMapUIComponent> MapUIComponent;
+
+	UPROPERTY()
+	TObjectPtr<UMaterialInstanceDynamic> MiniMapMID;
+
+	UPROPERTY()
+	TObjectPtr<UTexture2D> MapTexture;
+
+	float LevelDimension = 20000.f;
+	float MiniMapZoom = 0.2f;
+};
