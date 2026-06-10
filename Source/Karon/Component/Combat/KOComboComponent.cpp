0e2efb7cc6218de13d0927299642e464e858e837
NMEii <skaxo0823@gmail.com>
2026-06-10 21:32:51 +0900

    feat: ComboComponent 추가

---
+++ Source/Karon/Component/Combat/KOComboComponent.cpp
@@ -0,0 +1,99 @@
+﻿#include "KOComboComponent.h"
+
+#include "AbilitySystemGlobals.h"
+#include "AbilitySystem/KOAbilitySystemComponent.h"
+#include "AbilitySystem/Tag/Data/KOGameplayTags_Data.h"
+#include "AbilitySystem/Tag/Input/KOGameplayTags_Input.h"
+
+
+UKOComboComponent::UKOComboComponent()
+{
+	PrimaryComponentTick.bCanEverTick = true;
+
+}
+
+void UKOComboComponent::TickComponent(float DeltaTime, ELevelTick TickType,
+                                      FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	if (BufferInfo.BufferInput != EAttackInputType::None)
+	{
+		BufferInfo.RemainingTime -= DeltaTime;
+		
+		if (BufferInfo.RemainingTime <= 0.0f)
+		{
+			UE_LOG(LogTemp, Warning, TEXT("[ComboComponent] 선입력 버퍼 시간 만료."));
+			ResetBuffer();
+		}
+	}
+}
+
+void UKOComboComponent::RegisterInput(EAttackInputType InputType)
+{
+	bool bSuccess = TryExcuteAttack(InputType);
+	
+	if (bSuccess)
+	{
+		ResetBuffer();
+		UE_LOG(LogTemp, Warning, TEXT("[ComboComponent] 선입력 공격 실행 -> 버퍼 리셋"));
+	}
+	else
+	{
+		BufferInfo.BufferInput = InputType;
+		BufferInfo.RemainingTime = MaxBufferGraceTime;
+		UE_LOG(LogTemp, Warning, TEXT("[ComboComponent] 동작 중으로 인한 취소 -> 버퍼 리셋"));
+	}
+}
+
+void UKOComboComponent::ConsumeBuffer()
+{
+	if (BufferInfo.BufferInput != EAttackInputType::None)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("[ComboComponent] 저장되어 있는 버퍼 입력 사용"));
+		
+		EAttackInputType SavedInput = BufferInfo.BufferInput;
+		
+		ResetBuffer();
+		
+		RegisterInput(SavedInput);
+	}
+}
+
+void UKOComboComponent::ResetBuffer()
+{
+	BufferInfo.BufferInput = EAttackInputType::None;
+	BufferInfo.RemainingTime = 0.0f;
+}
+
+bool UKOComboComponent::TryExcuteAttack(EAttackInputType InputType)
+{
+	AActor* OwnerActor = GetOwner();
+	if (!OwnerActor)
+	{
+		return false;
+	}
+	
+	UAbilitySystemComponent* ASC = 
+		UAbilitySystemGlobals::GetAbilitySystemComponentFromActor(OwnerActor);
+	if (!ASC)
+	{
+		return false;
+	}
+	
+	bool bActivationSuccess = false;
+	
+	if (InputType == EAttackInputType::Light)
+	{
+		bActivationSuccess = 
+			ASC->TryActivateAbilitiesByTag(FGameplayTagContainer(KOGameplayTags::Input_Ability_Attack_Light));
+	}
+	else if (InputType == EAttackInputType::Heavy)
+	{
+		bActivationSuccess = 
+			ASC->TryActivateAbilitiesByTag(FGameplayTagContainer(KOGameplayTags::Input_Ability_Attack_Heavy));
+	}
+		
+	return bActivationSuccess;
+}
+
---
+++ Source/Karon/Component/Combat/KOComboComponent.h
@@ -0,0 +1,54 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "Data/KOComboActionData.h"
+#include "KOComboComponent.generated.h"
+
+enum EAttackInputType;
+
+USTRUCT(BlueprintType)
+struct FInputBufferInfo
+{
+	GENERATED_BODY()
+	
+	UPROPERTY(BlueprintReadWrite, Category="Combo Buffer")
+	EAttackInputType BufferInput = EAttackInputType::None;
+	
+	UPROPERTY(BlueprintReadWrite, Category="Combo Buffer")
+	float RemainingTime = 0.0f;
+};
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class KARON_API UKOComboComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	UKOComboComponent();
+	
+	virtual void TickComponent(
+		float DeltaTime,
+		ELevelTick TickType,
+	    FActorComponentTickFunction* ThisTickFunction
+	) override;
+	
+	UFUNCTION(BlueprintCallable, Category = "Combo System")
+	void RegisterInput(EAttackInputType InputType);
+	
+	UFUNCTION(BlueprintCallable, Category = "Combo System")
+	void ConsumeBuffer();
+	
+	UFUNCTION(BlueprintCallable, Category = "Combo System")
+	void ResetBuffer();
+	
+protected:
+	UPROPERTY(BlueprintReadWrite, Category = "Combo System|State")
+	FInputBufferInfo BufferInfo;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Combo System|Settings")
+	float MaxBufferGraceTime = 0.5f;
+	
+private:
+	bool TryExcuteAttack(EAttackInputType InputType);
+};
