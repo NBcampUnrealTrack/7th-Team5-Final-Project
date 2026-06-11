9cc1909e36a5fb945a8faf316424d2877c2b4b05
yuna1005 <yunaj9910@gmail.com>
2026-06-11 21:05:23 +0900

    feat:.karon_BP

---
+++ Source/Karon/AbilitySystem/Ability/Attack/KOGA_Attack_PlungeCharge.h
@@ -0,0 +1,34 @@
+// // Fill out your copyright notice in the Description page of Project Settings.
+//
+// #pragma once
+//
+// #include "CoreMinimal.h"
+// #include "AbilitySystem/Ability/Attack/KOGA_AttackBase.h"
+// #include "Abilities/Tasks/AbilityTask_PlayMontageAndWait.h"
+// #include "Abilities/Tasks/AbilityTask_WaitInputRelease.h"
+// #include "KOGA_Attack_PlungeCharge.generated.h"
+//
+//
+// UCLASS()
+// class KARON_API UKOGA_Attack_PlungeCharge : public UKOGA_AttackBase
+// {
+// 	GENERATED_BODY()
+// 	
+// public:
+// 	UKOGA_Attack_PlungeCharge();
+// 	
+// protected:
+// 	virtual void ActivateAbility(
+// 		const FGameplayAbilitySpecHandle Handle,
+// 		const FGameplayAbilityActorInfo* ActorInfo,
+// 		const FGameplayAbilityActivationInfo ActivationInfo,
+// 		const FGameplayEventData* TriggerEventData
+// 		)override;
+// 	
+// 	virtual void EndAbility(
+// 		const FGameplayAbilitySpecHandle Handle,
+// 		const FGameplayAbilityActorInfo* ActorInfo,
+// 		const FGameplayAbilityActivationInfo ActivationInfo,
+// 		bool bReplicatedEndAbility,
+// 		bool
+// };
