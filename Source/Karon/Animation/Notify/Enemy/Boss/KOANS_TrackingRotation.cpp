29a9bdf5d0e29b19b226f67130b6b4b5b2c5e4b1
Kim Taewoo <xodn246@gmail.com>
2026-06-16 02:04:51 +0900

    feat: 약 유도 공격용 notify state 추가

---
+++ Source/Karon/Animation/Notify/Enemy/Boss/KOANS_TrackingRotation.cpp
@@ -0,0 +1,59 @@
+#include "Animation/Notify/Enemy/Boss/KOANS_TrackingRotation.h"
+
+#include "AIController.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "Character/Enemy/Boss/KOAIC_BossChapter01.h"
+
+void UKOANS_TrackingRotation::NotifyTick(
+	USkeletalMeshComponent* MeshComp,
+	UAnimSequenceBase* Animation,
+	float FrameDeltaTime,
+	const FAnimNotifyEventReference& EventReference)
+{
+	if (!MeshComp || !MeshComp->GetOwner()) 
+	{
+		return; 
+	}
+ 
+	APawn* Pawn = Cast<APawn>(MeshComp->GetOwner());
+	if (!Pawn) 
+	{ 
+		return; 
+	}
+ 
+	AAIController* AIC = Cast<AAIController>(Pawn->GetController());
+	if (!AIC) 
+	{ 
+		return; 
+	}
+ 
+	UBlackboardComponent* BB = AIC->GetBlackboardComponent();
+	if (!BB) 
+	{ 
+		return; 
+	}
+ 
+	AActor* Target = Cast<AActor>(BB->GetValueAsObject(AKOAIC_BossChapter01::TargetActorKey));
+	if (!Target) 
+	{ 
+		return; 
+	}
+ 
+	// Z 무시하고 수평 방향만 회전
+	FVector Direction = Target->GetActorLocation()
+		- Pawn->GetActorLocation();
+	Direction.Z = 0.f;
+	if (Direction.IsNearlyZero()) 
+	{ 
+		return; 
+	}
+ 
+	FRotator NewRotation = FMath::RInterpTo(
+		Pawn->GetActorRotation(),
+		Direction.Rotation(),
+		FrameDeltaTime,
+		RotateSpeed
+	);
+ 
+	Pawn->SetActorRotation(NewRotation);
+}
