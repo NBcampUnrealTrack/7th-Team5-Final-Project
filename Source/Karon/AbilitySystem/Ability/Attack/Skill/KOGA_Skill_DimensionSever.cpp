c65ea4c8c44697f5ae181057a4b916785f8c48a2
NMEii <skaxo0823@gmail.com>
2026-06-17 20:50:01 +0900

    feat: 스킬(Dimension Sever) 추가

---
+++ Source/Karon/AbilitySystem/Ability/Attack/Skill/KOGA_Skill_DimensionSever.cpp
@@ -0,0 +1,47 @@
+﻿#include "KOGA_Skill_DimensionSever.h"
+
+#include "Abilities/Tasks/AbilityTask_PlayMontageAndWait.h"
+
+UKOGA_Skill_DimensionSever::UKOGA_Skill_DimensionSever()
+{
+}
+
+void UKOGA_Skill_DimensionSever::ExecuteAttack(float ChargePercentage)
+{
+	Super::ExecuteAttack(ChargePercentage);
+	
+	float DamageMultiplier = 0.3f + ChargePercentage;
+	
+	if (AttackMontage)
+	{
+		AttackMontageTask = UAbilityTask_PlayMontageAndWait::CreatePlayMontageAndWaitProxy(
+			this,
+			NAME_None,
+			AttackMontage,
+			1.0f,
+			NAME_None,
+			false
+		);
+		
+		if (AttackMontageTask)
+		{
+			AttackMontageTask->OnCompleted.AddDynamic(this, &ThisClass::UKOGA_Skill_DimensionSever::OnAttackMontageEnded);
+			AttackMontageTask->OnBlendOut.AddDynamic(this, &ThisClass::UKOGA_Skill_DimensionSever::OnAttackMontageEnded);
+			AttackMontageTask->OnInterrupted.AddDynamic(this, &ThisClass::UKOGA_Skill_DimensionSever::OnAttackMontageEnded);
+			AttackMontageTask->OnCancelled.AddDynamic(this, &ThisClass::UKOGA_Skill_DimensionSever::OnAttackMontageEnded);
+			
+			AttackMontageTask->ReadyForActivation();
+		}
+	}
+	else
+	{
+		UE_LOG(LogTemp, Error, TEXT("[%s] AttackMontage가 비어 있습니다! 에디터에서 설정해주세요."), *GetName());
+        EndAbility(CurrentSpecHandle, CurrentActorInfo, CurrentActivationInfo, true, false);
+	}
+	
+}
+
+void UKOGA_Skill_DimensionSever::OnAttackMontageEnded()
+{
+	EndAbility(CurrentSpecHandle, CurrentActorInfo, CurrentActivationInfo, true, false);
+}
---
+++ Source/Karon/AbilitySystem/Ability/Attack/Skill/KOGA_Skill_DimensionSever.h
@@ -0,0 +1,30 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystem/Ability/Attack/KOGA_Charge_Attack_Base.h"
+#include "KOGA_Skill_DimensionSever.generated.h"
+
+class UAnimMontage;
+class UAbilityTask_PlayMontageAndWait;
+
+UCLASS()
+class KARON_API UKOGA_Skill_DimensionSever : public UKOGA_Charge_Attack_Base
+{
+	GENERATED_BODY()
+	
+public:
+	UKOGA_Skill_DimensionSever();
+	
+protected:
+	virtual void ExecuteAttack(float ChargePercentage) override;
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Skill|Animation")
+	TObjectPtr<UAnimMontage> AttackMontage;
+	
+private:
+	UFUNCTION()
+	void OnAttackMontageEnded();
+	
+	UPROPERTY()
+	TObjectPtr<UAbilityTask_PlayMontageAndWait> AttackMontageTask;
+};
