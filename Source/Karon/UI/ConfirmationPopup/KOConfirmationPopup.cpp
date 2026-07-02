948fadb5966aeec19d37fe0fd98e204200579228
csh8696-git <csh8696@gmail.com>
2026-07-02 14:29:36 +0900

    [성훈] 임시 팝업 미완성본 추가
    [git-p4: depot-paths = "//nbc_karon/main/Karon/": change = 159]

---
+++ Source/Karon/UI/ConfirmationPopup/KOConfirmationPopup.cpp
@@ -0,0 +1,20 @@
+﻿// Copyright Karon Team 5. All Rights Reserved.
+
+#include "KOConfirmationPopup.h"
+
+#include "CommonButtonBase.h"
+
+void UKOConfirmationPopup::NativeConstruct()
+{
+	Super::NativeConstruct();
+	
+	if (CloseButton)
+	{
+		CloseButton->OnClicked().AddUObject(this, &UKOConfirmationPopup::OnCloseButtonClicked);
+	}
+}
+
+void UKOConfirmationPopup::OnCloseButtonClicked()
+{
+	
+}
---
+++ Source/Karon/UI/ConfirmationPopup/KOConfirmationPopup.h
@@ -0,0 +1,36 @@
+﻿// Copyright Karon Team 5. All Rights Reserved.
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UI/KOActivatableWidget.h"
+#include "KOConfirmationPopup.generated.h"
+
+class UCommonTextBlock;
+class UCommonButtonBase;
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnPopupClosedSignature);
+/**
+ * 레벨 전환 전 재확인 하는 팝업 창
+ */
+UCLASS()
+class KARON_API UKOConfirmationPopup : public UKOActivatableWidget
+{
+	GENERATED_BODY()
+	
+public:
+	UPROPERTY()
+	FOnPopupClosedSignature OnConfirmed;
+	
+	virtual void NativeConstruct() override;
+	
+	void SetupPopup(const FText& Title, const FText& Description);
+	
+protected:
+	void HandleConfirm();
+	
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UCommonButtonBase> CloseButton;
+	
+private:
+	void OnCloseButtonClicked();
+};
