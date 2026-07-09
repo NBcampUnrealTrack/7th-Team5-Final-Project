8b53812ec71e29e1241ea6e42ae7448e678ae1bf
khs081215 <khs081215@naver.com>
2026-07-09 17:42:28 +0900

    [현석] GMRouter 콘솔 명령어 기반 디버그 로깅 시스템 추가
    [git-p4: depot-paths = "//nbc_karon/main/Karon/": change = 214]

--- Plugins/GMRouter/Source/GMRouter/Private/GMRouterSubsystem.cpp
+++ Plugins/GMRouter/Source/GMRouter/Private/GMRouterSubsystem.cpp
@@ -2,6 +2,14 @@
 
 #include "GMRouterSubsystem.h"
 
+static TAutoConsoleVariable<int32> CVarLogGMRouter(
+	TEXT("gmrouter.LogMessages"), // 콘솔창에 입력할 명령어
+	0,                            // 기본값 (0: 비활성화, 1: 활성화)
+	TEXT("GMRouter의 메시지 발행(Broadcast) 및 구독 상황을 실시간으로 로그에 출력합니다.\n")
+	TEXT("0: 비활성화, 1: 활성화"),
+	ECVF_Cheat                    // 치트 명령어로 분류 (배포 빌드 제어 가능)
+);
+
 //friend class로 Unsubscribe를 호출
 void FGameplayMessageHandle::Unsubscribe()
 {
@@ -15,6 +23,30 @@ void FGameplayMessageHandle::Unsubscribe()
 
 void UGMRouterSubsystem::BroadcastMessage(const FGameplayTag& Channel, const FInstancedStruct& Payload)
 {
+	if (CVarLogGMRouter.GetValueOnGameThread() == 1)
+	{
+		// 현재 프로젝트에서 틱마다 Event.Interaction을 쏘기때문에 임시 로깅 제거
+		if (Channel!=FGameplayTag::RequestGameplayTag(TEXT("Event.Interaction")))
+		{
+			// 로그창(Output Log)에 상세 정보 출력
+			UE_LOG(LogTemp, Log, TEXT("[GMRouter] Broadcast -> Channel: %s | Payload Type: %s"), 
+				*Channel.ToString(), 
+				Payload.IsValid() ? *Payload.GetScriptStruct()->GetName() : TEXT("None"));
+
+			// 플레이 중인 화면(Viewport) 좌측 상단에 실시간으로 출려하여 기획/아트가 바로 보게 함
+			if (GEngine)
+			{
+				FString DebugMessage = FString::Printf(TEXT("[GMRouter] Channel: %s (%s)"), 
+					*Channel.ToString(), 
+					Payload.IsValid() ? *Payload.GetScriptStruct()->GetName() : TEXT("None"));
+            
+				// 5초 동안 연두색(Cyan)으로 화면에 표시
+				GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Cyan, DebugMessage);
+			}
+		}
+
+	}
+	
 	if (auto ListenerList = ListenerMap.Find(Channel))
 	{
 		//콜백 함수가 실행되는 중 컨테이너의 수정이 일어날 경우 크래시 발생 위험으로 Snapshot 설정
