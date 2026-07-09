0d8603f67b34f37127f4706f8e3b10b13764becf
yuna1005 <yunaj9910@gmail.com>
2026-07-09 22:40:33 +0900

    [유나] 뮤터블 누락본
    [git-p4: depot-paths = "//nbc_karon/main/Karon/": change = 220]

---
+++ Source/Karon/Game/KO_OutFitController.h
@@ -0,0 +1,20 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "KO_OutFitController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class KARON_API AKO_OutFitController : public APlayerController
+{
+	GENERATED_BODY()
+	
+protected:
+	virtual void BeginPlay() override;
+	
+};
