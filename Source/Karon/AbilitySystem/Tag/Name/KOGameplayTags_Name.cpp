b802d9fff5faf36068ade5dca2889ba28accabbb
khs081215 <khs081215@naver.com>
2026-07-09 09:17:39 +0900

    [현석] 정예 에너미 추가
    [git-p4: depot-paths = "//nbc_karon/main/Karon/": change = 209]

--- Source/Karon/AbilitySystem/Tag/Name/KOGameplayTags_Name.cpp
+++ Source/Karon/AbilitySystem/Tag/Name/KOGameplayTags_Name.cpp
@@ -6,4 +6,5 @@ namespace KOGameplayTags
 	UE_DEFINE_GAMEPLAY_TAG(Name_Enemy_Warrior, "Name.Enemy.Warrior");
 	UE_DEFINE_GAMEPLAY_TAG(Name_Enemy_Drone, "Name.Enemy.Drone");
 	UE_DEFINE_GAMEPLAY_TAG(Name_Enemy_Tanker, "Name.Enemy.Tanker");
+	UE_DEFINE_GAMEPLAY_TAG(Name_Enemy_Elite, "Name.Enemy.Elite");
 }
--- Source/Karon/AbilitySystem/Tag/Name/KOGameplayTags_Name.h
+++ Source/Karon/AbilitySystem/Tag/Name/KOGameplayTags_Name.h
@@ -8,4 +8,5 @@ namespace KOGameplayTags
 	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Name_Enemy_Warrior);
 	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Name_Enemy_Drone);
 	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Name_Enemy_Tanker);
+	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Name_Enemy_Elite);
 }
