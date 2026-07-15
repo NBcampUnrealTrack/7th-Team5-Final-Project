fbc7545e1ec0ea76e6f3bd14cfd00e6af393cd2d
khs081215 <khs081215@naver.com>
2026-07-16 00:32:35 +0900

    [현석] 텔레포트 액터 생성 / 레벨 에셋 추가 배치
    [git-p4: depot-paths = "//nbc_karon/main/Karon/": change = 286]

---
+++ Source/Karon/MapActor/KOTeleportActor.cpp
@@ -0,0 +1,40 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "KOTeleportActor.h"
+
+#include "Kismet/GameplayStatics.h"
+
+
+// Sets default values
+AKOTeleportActor::AKOTeleportActor()
+{
+	
+}
+
+bool AKOTeleportActor::CanInteract(AActor* Interactor) const
+{
+	return true;
+}
+
+void AKOTeleportActor::OnInteract(AActor* Interactor)
+{
+	if (IsValid(TargetTeleportActor))
+	{
+		APawn* Player=UGameplayStatics::GetPlayerPawn(this, 0);
+		if (Player)
+		{
+			Player->SetActorLocation(TargetTeleportActor->GetActorLocation());
+		}
+	}
+	else
+	{
+		return;
+	}
+}
+
+FText AKOTeleportActor::GetInteractionPrompt() const
+{
+	return FText::FromString(TEXT("이동하기"));
+}
+
