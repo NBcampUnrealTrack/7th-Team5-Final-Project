bc224248ed2c20c52876299de7c1fe288b452f37
khs081215 <khs081215@naver.com>
2026-07-20 04:09:33 +0900

    [현석] 사운드 에셋 추가/ 에너미 및 레벨시퀀스 사운드 추가
    [git-p4: depot-paths = "//nbc_karon/main/Karon/": change = 338]

---
+++ Source/Karon/Animation/Notify/KOSoundSpawnNotify.cpp
@@ -0,0 +1,40 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "KOSoundSpawnNotify.h"
+
+#include "Kismet/GameplayStatics.h"
+
+UKOSoundSpawnNotify::UKOSoundSpawnNotify()
+{
+	bIsNativeBranchingPoint=true;
+}
+
+void UKOSoundSpawnNotify::Notify(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation,
+	const FAnimNotifyEventReference& EventReference)
+{
+	Super::Notify(MeshComp, Animation, EventReference);
+
+	if (!SoundBase)
+	{
+		return;
+	}
+	
+	if (MeshComp)
+	{
+		UWorld* World = MeshComp->GetWorld();
+		if (World&&MeshComp->GetOwner())
+		{
+			UGameplayStatics::PlaySoundAtLocation(
+				World, 
+				SoundBase, 
+				MeshComp->GetOwner()->GetActorLocation(), 
+				FRotator::ZeroRotator, 
+				Volume,              
+				PlayRate,                  
+				StartTime                
+			);
+		}
+	}
+}
+
---
+++ Source/Karon/Animation/Notify/KOSoundSpawnNotify.h
@@ -0,0 +1,35 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimNotifies/AnimNotify.h"
+#include "KOSoundSpawnNotify.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class KARON_API UKOSoundSpawnNotify : public UAnimNotify
+{
+	GENERATED_BODY()
+	UKOSoundSpawnNotify();
+	
+public:
+	virtual void Notify(USkeletalMeshComponent* MeshComp, 
+	UAnimSequenceBase* Animation, 
+	const FAnimNotifyEventReference& EventReference) override;
+	
+protected:
+	UPROPERTY(EditAnywhere)
+	TObjectPtr<USoundBase> SoundBase;
+	
+	UPROPERTY(EditAnywhere)
+	float Volume=1.f;
+	
+	UPROPERTY(EditAnywhere)
+	float PlayRate=1.f;
+	
+	UPROPERTY(EditAnywhere)
+	float StartTime=0.f;
+};
