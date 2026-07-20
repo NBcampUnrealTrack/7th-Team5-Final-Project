5f7cf49e2e9ead0cc18550974df0e7aea51f3094
Kim Taewoo <xodn246@gmail.com>
2026-07-21 02:07:05 +0900

    [태우] 보스 기믹 기둥 파괴시 파편화 추가 보스 기믹 기둥 파괴 VFX 추가
    [git-p4: depot-paths = "//nbc_karon/main/Karon/": change = 363]

--- Source/Karon/Character/Enemy/Boss/Chapter01/Gimmick/KOBossCH01GimmickPillar.h
+++ Source/Karon/Character/Enemy/Boss/Chapter01/Gimmick/KOBossCH01GimmickPillar.h
@@ -4,6 +4,8 @@
 #include "GameFramework/Actor.h"
 #include "KOBossCH01GimmickPillar.generated.h"
 
+class UNiagaraSystem;
+
 UCLASS()
 class KARON_API AKOBossCH01GimmickPillar : public AActor
 {
@@ -24,8 +26,11 @@ public:
 	
 protected:
 	virtual void BeginPlay() override;
+	virtual void Tick(float DeltaTime) override;
 	
 	void ApplyBrokenState();
+	void ExplodeDebris();
+	void UpdateDebrisFade(float DeltaTime);
  
 protected:
 	UPROPERTY(VisibleAnywhere, Category = "Pillar | Component")
@@ -37,4 +42,41 @@ protected:
 	
 	UPROPERTY(VisibleInstanceOnly, BlueprintReadOnly, Category = "Pillar|Save")
 	bool bBroken = false;
+	
+	// VFX
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Pillar | Effect")
+	TObjectPtr<UNiagaraSystem> DustEffect;
+	
+	// 파편화
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Pillar | Break")
+	TObjectPtr<UStaticMesh> DebrisMesh;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Pillar | Break")
+	int32 DebrisMinCount = 5;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Pillar | Break")
+	int32 DebrisMaxCount = 9;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Pillar | Break")
+	FVector2D DebrisScaleRange = FVector2D(0.12f, 0.28f);
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Pillar | Break")
+	float DebrisImpulseStrength = 250.f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Pillar | Break")
+	float DebrisLifeSpan = 2.f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Pillar | Break")
+	float DebrisFadeOutDuration = 0.6f;
+
+private:
+	struct FDebrisFadeInfo
+	{
+		TWeakObjectPtr<AStaticMeshActor> DebrisActor;
+		FVector InitialScale = FVector::OneVector;
+		float ElapsedTime = 0.f;
+		bool bFadeStarted = false;
+	};
+
+	TArray<FDebrisFadeInfo> FadingDebris;
 };
