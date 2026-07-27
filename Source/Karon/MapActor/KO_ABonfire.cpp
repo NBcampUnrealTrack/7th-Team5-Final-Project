26eb6ce61157fda330b42659ad548409504450ec
NMEii <skaxo0823@gmail.com>
2026-07-27 15:43:59 +0900

    [남태] 화톳불 기본 엑터 추가
    [git-p4: depot-paths = "//nbc_karon/main/Karon/": change = 404]

---
+++ Source/Karon/MapActor/KO_ABonfire.cpp
@@ -0,0 +1,59 @@
+﻿#include "KO_ABonfire.h"
+
+#include "Components/BoxComponent.h"
+
+
+AKO_ABonfire::AKO_ABonfire()
+{
+	PrimaryActorTick.bCanEverTick = false;
+	
+	RootComp = CreateDefaultSubobject<USceneComponent>(TEXT("RootComp"));
+	RootComponent = RootComp;
+
+	BonfireMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("BonfireMesh"));
+	BonfireMesh->SetupAttachment(RootComponent);
+    
+	BonfireMesh->SetCollisionProfileName(TEXT("BlockAllDynamic"));
+
+	InteractionBox = CreateDefaultSubobject<UBoxComponent>(TEXT("InteractionBox"));
+	InteractionBox->SetupAttachment(RootComponent);
+	InteractionBox->SetBoxExtent(FVector(150.f, 150.f, 100.f));
+	
+	bIsActivated = false;
+	BonfireID = NAME_None;
+}
+
+void AKO_ABonfire::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	if (BonfireID == NAME_None)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Bonfire ID 설정 안됨."));
+	}
+	
+	LoadBonfireState();
+}
+
+void AKO_ABonfire::Interact()
+{
+	if (!bIsActivated)
+	{
+		bIsActivated = true;
+		SaveBonfireState();
+	}
+	else
+	{
+		// TODO: 화톳불 UI 띄우기
+	}
+}
+
+void AKO_ABonfire::SaveBonfireState()
+{
+}
+
+void AKO_ABonfire::LoadBonfireState()
+{
+}
+
+
---
+++ Source/Karon/MapActor/KO_ABonfire.h
@@ -0,0 +1,49 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "KO_ABonfire.generated.h"
+
+class UBoxComponent;
+class UBoxComponent;
+class UStaticMeshComponent;
+
+UCLASS()
+class KARON_API AKO_ABonfire : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	AKO_ABonfire();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	UFUNCTION(BlueprintCallable, Category = "Bonfire")
+	void Interact();
+	
+protected:
+	void SaveBonfireState();
+	
+	void LoadBonfireState();
+	
+protected:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+	TObjectPtr<USceneComponent> RootComp;
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+	TObjectPtr<UStaticMeshComponent> BonfireMesh;
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+	TObjectPtr<UBoxComponent> InteractionBox;
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+	TObjectPtr<USceneComponent> TeleportTargetComponent;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Bonfire Setting")
+	FName BonfireID;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Bonfire Setting")
+	bool bIsActivated;
+};
